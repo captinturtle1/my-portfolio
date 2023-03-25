6c14725682f60e71f64bd54c6263d62914917ce5
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2023-03-25 15:49:04 -0700

    Project descriptions

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -30,22 +30,22 @@ export default function Projects() {
                     <div onClick={() => handleExpansion(index)} className={
                         project.name == 'Abyss' ? 
                             (expanded == index ? 
-                                'mx-auto bg-[#E2C05E] lg:w-[800px] h-[650px] bg-opacity-100 border p-4 border-[#E2C05E] rounded-3xl rounded-tl-[30px] cursor-pointer transition-all' : 
+                                'mx-auto bg-[#E2C05E] lg:w-[800px] h-[730px] lg:h-[680px] bg-opacity-100 border p-4 border-[#E2C05E] rounded-3xl rounded-tl-[30px] cursor-pointer transition-all' : 
                                 'mx-auto bg-[#E2C05E] lg:w-[800px] h-32 lg:h-[250px] bg-opacity-50 hover:bg-opacity-60 backdrop-blur-lg border p-4 border-[#E2C05E] rounded-3xl rounded-l-[30px] cursor-pointer transition-all'
                             ) :
                             project.name == 'Minty' ? 
                                 (expanded == index ? 
-                                    'mx-auto bg-[#305187] lg:w-[800px] h-[620px] bg-opacity-100 border p-4 border-[#305187] rounded-3xl rounded-tl-[30px] cursor-pointer transition-all' : 
+                                    'mx-auto bg-[#305187] lg:w-[800px] h-[650px] lg:h-[620px] bg-opacity-100 border p-4 border-[#305187] rounded-3xl rounded-tl-[30px] cursor-pointer transition-all' : 
                                     'mx-auto bg-[#305187] lg:w-[800px] h-32 lg:h-[250px] bg-opacity-50 hover:bg-opacity-60 backdrop-blur-lg border p-4 border-[#305187] rounded-3xl rounded-l-[30px] cursor-pointer transition-all'
                                 ) :
                                 project.name == 'JPEG Squared' ? 
                                     (expanded == index ? 
-                                        'mx-auto bg-[#2E0910] lg:w-[800px] h-[620px] bg-opacity-100 border p-4 border-[#2E0910] rounded-3xl rounded-tl-[30px] cursor-pointer transition-all' : 
+                                        'mx-auto bg-[#2E0910] lg:w-[800px] h-[530px] bg-opacity-100 border p-4 border-[#2E0910] rounded-3xl rounded-tl-[30px] cursor-pointer transition-all' : 
                                         'mx-auto bg-[#2E0910] lg:w-[800px] h-32 lg:h-[250px] bg-opacity-50 hover:bg-opacity-60 backdrop-blur-lg border p-4 border-[#2E0910] rounded-3xl rounded-l-[30px] cursor-pointer transition-all'
                                     ) :
                                     project.name == 'Yogurt Verse' ? 
                                         (expanded == index ? 
-                                            'mx-auto bg-[#D1737F] lg:w-[800px] h-[650px] bg-opacity-100 border p-4 border-[#D1737F] rounded-3xl rounded-tl-[30px] cursor-pointer transition-all' : 
+                                            'mx-auto bg-[#D1737F] lg:w-[800px] h-[750px] lg:h-[700px] bg-opacity-100 border p-4 border-[#D1737F] rounded-3xl rounded-tl-[30px] cursor-pointer transition-all' : 
                                             'mx-auto bg-[#D1737F] lg:w-[800px] h-32 lg:h-[250px] bg-opacity-50 hover:bg-opacity-60 backdrop-blur-lg border p-4 border-[#D1737F] rounded-3xl rounded-l-[30px] cursor-pointer transition-all'
                                         ) :
                                         (expanded == index ? 
@@ -59,10 +59,8 @@ export default function Projects() {
                                 <div className='text-sm md:text-lg lg:text-2xl font-semibold'>{project.tags}</div>
                             </div>
                         </div>
-                        <div className={expanded == index ? 'mx-16 mt-16 text-sm  md:text-xl visible opacity-100 transition-all duration-300' : 'mx-16 mt-16 text-sm lg:text-xl invisible opacity-0 transition-all -translate-y-16 duration-75'}>
-                            <div>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </div>
+                        <div className={expanded == index ? 'mx-16 mt-8 text-sm  md:text-xl visible opacity-100 transition-all duration-300' : 'mx-16 mt-16 text-sm lg:text-xl invisible opacity-0 transition-all -translate-y-16 duration-75'}>
+                            <div className='whitespace-pre-wrap'>{project.desc}</div>
                             {project.websiteURL != '' ? (
                                 <div className='flex mt-8 font-bold text-lg'>
                                     <a href={project.websiteURL} className='flex translate-x-0 hover:translate-x-2 transition-all'>
--- src/components/projectDetails.tsx
+++ src/components/projectDetails.tsx
@@ -7,28 +7,29 @@ export const projectDetails = [
     {
         name: 'Minty',
         logo: mintyLogo,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 
+        `Minty is a desktop application that helps with Ethereum wallet automation. Includes a wallet manager that lets you create wallets and consolidate/disperse funds between them easily.\n\nIncludes a task system that allows you to create a group of custom transactions to get automatically sent.\n\nHas a built in stats tracker that will gather profit/loss of a given collection with the wallets stored in the app. \n\nBuilt with Electron, React, Tailwindcss, and Ethersjs`,
         tags: 'Front-end · Back-end',
         websiteURL: ''
     },
     {
         name: 'Abyss',
         logo: abyssLogo,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Built a single page website including information about the group. Built a user dashboard that allows users to check their subscription expiration, and renew.\n\nBuilt back-end membership contract that keeps allows members to pay for membership and for funds to be withdrawn by contract owner.\n\nBuilt Back-end API calls contract to get expiration status of users and automatically change roles in Discord group.\n\nBuilt with NextJS, Tailwindcss, Solidity, and Ethersjs',
         tags: 'Front-end · Back-end',
         websiteURL: 'https://www.abyssfnf.com/'
     },
     {
         name: 'Yogurt Verse',
         logo: yvLogo,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Built single page website that gives information about the project\n\nBuilt user dashboard to let users view their inventory. Dashboard allows staking and unstaking via staking contract.\n\nCreated main contract and staking contract to allow users to mint and then stake through the user dashboard.\n\nBuilt custom Discord bot to check user staked time accross items and add roles depending on staked time.\n\nBuilt with NextJS, Tailwindcss, Solidity and Ethersjs',
         tags: 'Front-end · Back-end',
         websiteURL: 'https://www.yogurtverse.xyz/'
     },
     {
         name: 'JPEG Squared',
         logo: jsLogo,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Created single page website to display infromation about group. Website pulled from Opensea API to display an up to date auction. Allows pass purchase through website that talks to back-end contract.\n\nCreated contract to purchase member passes to authenticate users in a Discord group.\n\nBuilt with React, Web3js, and Solidity',
         tags: 'Front-end · Back-end',
         websiteURL: ''
     },
