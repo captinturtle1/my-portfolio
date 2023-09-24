48250cae0c99fef55f3fb2e50561ed136681038f
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2023-09-24 01:36:02 -0600

    mobile spacing

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,13 +6,13 @@ import pfp from '../../public/me.jpg';
 
 export default function Home() {
   return (
-    <div className='w-screen flex p-32 bg-red-400 text-white'>
+    <div className='w-screen flex py-8 lg:p-32 bg-red-400 text-white'>
       <div className='m-auto flex flex-col lg:flex-row gap-16'>
-        <Image alt='Profile Picture' quality={100} src={pfp} className='rounded-2xl w-96 drop-shadow-lg m-auto lg:m-auto'/>
+        <Image alt='Profile Picture' quality={100} src={pfp} className='rounded-2xl w-[80%] lg:w-96 drop-shadow-lg m-auto lg:m-auto'/>
         <div className='flex flex-col gap-5 my-auto max-w-[600px]'>
           <h1 className='text-5xl font-bold m-auto lg:m-0'>Hello👋</h1>
-          <p className='text-lg text-center lg:text-left p-10 lg:p-0'>Hi, I&apos;m Austin Lopez, a full-stack developer with a passion for creating dynamic and intuitive web applications. With several years of experience in both front-end and back-end development, I have the skills and knowledge necessary to take a project from concept to completion.</p>
-          <p className='text-lg text-center lg:text-left p-10 lg:p-0'>I have several years of experience in both front-end and back-end development. My technical skills include proficiency in programming languages such as JavaScript, Typescript, HTML, CSS, as well as experience with popular frameworks and libraries like React, Node.js, Next.js, and Tailwindcss.</p>
+          <p className='text-lg text-center lg:text-left px-10 lg:p-0'>Hi, I&apos;m Austin Lopez, a full-stack developer with a passion for creating dynamic and intuitive web applications. With several years of experience in both front-end and back-end development, I have the skills and knowledge necessary to take a project from concept to completion.</p>
+          <p className='text-lg text-center lg:text-left px-10 lg:p-0'>I have several years of experience in both front-end and back-end development. My technical skills include proficiency in programming languages such as JavaScript, Typescript, HTML, CSS, as well as experience with popular frameworks and libraries like React, Node.js, Next.js, and Tailwindcss.</p>
           <button type='button' className='m-auto lg:m-0 flex text-white hover:translate-x-1 transition-all'>
             <a href='https://github.com/captinturtle1' target='_blank' className='text-2xl cursor-pointer flex'>View Github <BiRightArrowAlt className='my-[5px]'/></a>
           </button>
--- src/components/projectDetails.tsx
+++ src/components/projectDetails.tsx
@@ -6,6 +6,14 @@ import otkLogo from '../../public/otkLogo.jpg';
 
 export const projectDetails = [
     {
+        name: 'Is Anyone Live',
+        logo: whoisliveLogo,
+        desc: 'Is Anyone Live is a website that allows users to create lists of streamers across Twitch, Youtube, and Kick. Users have a dashboard to see which of their favorite streamers are currently live and various other details such as view count, current title, and a link to the stream. \n\nThe website uses an AWS Lambda API that uses Puppeteer to scrape needed data from various pages to display on the dashboard.',
+        tags: 'Javascript · React · Puppeteer · AWS Lambda',
+        websiteURL: 'https://isanyone.live/',
+        repoURL: 'https://github.com/captinturtle1/who_is_live_app'
+    },
+    {
         name: 'Minty',
         logo: mintyLogo,
         desc: 'Minty is a desktop application that helps with Ethereum wallet automation. Includes a wallet manager that lets you create wallets and consolidate/disperse funds between them easily.\n\nIncludes a task system that allows you to create a batch of transactions to get automatically sent.\n\nHas a built-in profit/loss tracker that will gather information of a given collection with the wallets stored in the app.',
@@ -20,14 +28,6 @@ export const projectDetails = [
         tags: 'Javascript · React · NextJS · AWS Amplify',
         websiteURL: 'https://otk.lol/',
         repoURL: 'https://github.com/captinturtle1/otk-website'
-    },
-    {
-        name: 'Is Anyone Live',
-        logo: whoisliveLogo,
-        desc: 'Is Anyone Live is a website that allows users to create lists of streamers across Twitch, Youtube, and Kick. Users have a dashboard to see which of their favorite streamers are currently live and various other details such as view count, current title, and a link to the stream. \n\nThe website uses a custom Express API that uses Puppeteer to scrape needed data from various pages to display on the dashboard.',
-        tags: 'Javascript · React · ExpressJS · Puppeteer · AWS EC2 ',
-        websiteURL: 'https://isanyone.live/',
-        repoURL: 'https://github.com/captinturtle1/who_is_live_app'
     },
     {
         name: 'Abyss',
